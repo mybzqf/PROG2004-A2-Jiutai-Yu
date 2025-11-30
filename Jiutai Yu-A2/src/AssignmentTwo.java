716e606548bd070eb7a2e8c38fad03a73639d751
mybzqf <y3534585883@163.com>
2025-12-01 02:38:47 +0800

    Add files via upload

--- Jiutai Yu-A2/src/AssignmentTwo.java
+++ Jiutai Yu-A2/src/AssignmentTwo.java
@@ -1,24 +1,48 @@
 public class AssignmentTwo {
-    // 主方法，程序入口
     public static void main(String[] args) {
-        // 后续可在此调用各part方法进行整体测试
+        AssignmentTwo app = new AssignmentTwo();
+        app.partThree();
     }
 
-    // Part3方法占位符
-    public void partThree() {}
+    // Part3: Waiting Line Test
+    public void partThree() {
+        System.out.println("=== Part3 Waiting Line Test ===");
 
-    // Part4A方法占位符
+        // 1. Create an operator
+        Employee op = new Employee("Zhang San", 30, "44010119950101", "EMP001", "Morning Shift");
+
+        // 2. Create a ride
+        Ride rollerCoaster = new Ride(op, "Roller Coaster", "Thrilling");
+
+        // 3. Create 5 visitors
+        Visitor v1 = new Visitor("Li Si", 25, "44010119980101", "T001", "2025-12-01");
+        Visitor v2 = new Visitor("Wang Wu", 32, "44010119910101", "T002", "2025-12-01");
+        Visitor v3 = new Visitor("Zhao Liu", 18, "44010120060101", "T003", "2025-12-01");
+        Visitor v4 = new Visitor("Sun Qi", 45, "44010119800101", "T004", "2025-12-01");
+        Visitor v5 = new Visitor("Zhou Ba", 22, "44010120020101", "T005", "2025-12-01");
+
+        // 4. Add visitors to the waiting line
+        rollerCoaster.addVisitorToQueue(v1);
+        rollerCoaster.addVisitorToQueue(v2);
+        rollerCoaster.addVisitorToQueue(v3);
+        rollerCoaster.addVisitorToQueue(v4);
+        rollerCoaster.addVisitorToQueue(v5);
+
+        // 5. Print the waiting line
+        rollerCoaster.printQueue();
+
+        // 6. Remove one visitor from the line
+        rollerCoaster.removeVisitorFromQueue();
+
+        // 7. Print the line again to verify removal
+        System.out.println("\nAfter removal:");
+        rollerCoaster.printQueue();
+    }
+
+    // Placeholders for other part methods (to be filled later)
     public void partFourA() {}
-
-    // Part4B方法占位符
     public void partFourB() {}
-
-    // Part6方法占位符
     public void partSix() {}
-
-    // Part7方法占位符
     public void partSeven() {}
-
-    // Part5方法占位符
     public void partFive() {}
 }
--- Jiutai Yu-A2/src/Ride.java
+++ Jiutai Yu-A2/src/Ride.java
@@ -1,56 +1,30 @@
-public class Ride implements RideInterface{
-    @Override
-    public void addVisitorToQueue(Visitor visitor) {
+import java.util.LinkedList;
+import java.util.Queue;
 
-    }
-
-    @Override
-    public void removeVisitorFromQueue() {
-
-    }
-
-    @Override
-    public void printQueue() {
-
-    }
-
-    @Override
-    public void addVisitorToHistory(Visitor visitor) {
-
-    }
-
-    @Override
-    public boolean checkVisitorFromHistory(Visitor visitor) {
-        return false;
-    }
-
-    @Override
-    public int numberOfVisitors() {
-        return 0;
-    }
-
-    @Override
-    public void printRideHistory() {
-
-    }
-
-    @Override
-    public void runOneCycle() {
-
-    }
-
+public class Ride implements RideInterface {
+    // Basic attributes (defined in Part1)
     private Employee operator;
     private String rideName;
     private String rideType;
 
-    public Ride() {}
+    // Part3: Waiting line (stores visitors waiting for the ride)
+    private Queue<Visitor> waitingLine;
+
+
+    // Constructors
+    public Ride() {
+        this.waitingLine = new LinkedList<>();
+    }
 
     public Ride(Employee operator, String rideName, String rideType) {
         this.operator = operator;
         this.rideName = rideName;
         this.rideType = rideType;
+        this.waitingLine = new LinkedList<>();
     }
 
+
+    // Getters & Setters for basic attributes
     public Employee getOperator() {
         return operator;
     }
@@ -73,5 +47,68 @@ public class Ride implements RideInterface{
 
     public void setRideType(String rideType) {
         this.rideType = rideType;
+    }
+
+
+    // Part3: Implement waiting line methods
+    @Override
+    public void addVisitorToQueue(Visitor visitor) {
+        if (visitor != null) {
+            waitingLine.offer(visitor);
+            System.out.println("Successfully added visitor [" + visitor.getName() + "] to the waiting line of [" + rideName + "]");
+        } else {
+            System.out.println("Failed to add: Visitor information is empty");
+        }
+    }
+
+    @Override
+    public void removeVisitorFromQueue() {
+        if (waitingLine.isEmpty()) {
+            System.out.println("Failed to remove: The waiting line of [" + rideName + "] is empty");
+        } else {
+            Visitor removedVisitor = waitingLine.poll();
+            System.out.println("Successfully removed visitor [" + removedVisitor.getName() + "] from the waiting line of [" + rideName + "]");
+        }
+    }
+
+    @Override
+    public void printQueue() {
+        System.out.println("=== Waiting Line Details of [" + rideName + "] ===");
+        if (waitingLine.isEmpty()) {
+            System.out.println("No visitors in the waiting line");
+            return;
+        }
+        int index = 1;
+        for (Visitor visitor : waitingLine) {
+            System.out.println(index + ". Name: " + visitor.getName()
+                    + " | Age: " + visitor.getAge()
+                    + " | Ticket ID: " + visitor.getVisitorTicketId());
+            index++;
+        }
+        System.out.println("==============================");
+    }
+
+
+    // Empty implementations for other interface methods (to be filled in later parts)
+    @Override
+    public void addVisitorToHistory(Visitor visitor) {
+    }
+
+    @Override
+    public boolean checkVisitorFromHistory(Visitor visitor) {
+        return false;
+    }
+
+    @Override
+    public int numberOfVisitors() {
+        return 0;
+    }
+
+    @Override
+    public void printRideHistory() {
+    }
+
+    @Override
+    public void runOneCycle() {
     }
 }
